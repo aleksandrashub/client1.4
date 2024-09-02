aa91078d70e9a6477bacdb9682514779cae328cc
dmitiry <you@example.com>
2024-09-02 16:22:34 +0300

    first commit

---
+++ shubenokClient/MainWindow.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace shubenokClient
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void AddClient_OnClick(object? sender, RoutedEventArgs args)
+        {
+            
+        }
+
+    }
+}
